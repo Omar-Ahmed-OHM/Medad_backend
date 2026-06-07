2a283183eb3081e2b46ef277de6c468ea7041649
Omar-Ahmed-Omar <omar.ahmed@mogo-eg.com>
2026-06-07 11:20:09 +0300

    Upload services for upload

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,19 @@ import mongoose from 'mongoose';
 
 const dbconnenction = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        if (!process.env.MONGO_URI) {
+            console.error("MONGO_URI is undefined! Check Vercel Settings.");
+            return;
+        }
+        
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        });
+        
         console.log('Database connected successfully');
     }
     catch (error) {
-        console.log('Error connecting to database:', error);
+        console.error('Error connecting to database:', error.message);
     }
 }
 export default dbconnenction;
