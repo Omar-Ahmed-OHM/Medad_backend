a93f97f026c73697e248d03fe8f6bdd307755b0f
Omar-Ahmed-Omar <omar.ahmed@mogo-eg.com>
2026-06-07 10:06:35 +0300

    make change

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 import express from 'express';
-import dbconnenction from './src/config/db.js';
 import dotenv from 'dotenv';
+import cors from "cors";
+
+import dbconnenction from './src/config/db.js';
 import hadithRoutes from './src/routes/hadith.routes.js';
 import zekrRoutes from './src/routes/zekr.routes.js';
-import cors from "cors";
-///////////////////////////
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+
 dotenv.config();
-dbconnenction();
-const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/hadith', hadithRoutes);
 app.use('/api/zekr', zekrRoutes);
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
 
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+const startServer = async () => {
+  try {
+    await dbconnenction();
+
+   app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+  } catch (err) {
+    console.error("DB connection failed:", err);
+  }
+};
+
+startServer();
